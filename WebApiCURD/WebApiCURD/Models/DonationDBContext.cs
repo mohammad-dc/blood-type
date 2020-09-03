43da3166204311e5ad6a4b5ebdbbfed509d77f74
mohammad ahamd <ma3571318z@gmail.com>
2020-09-03 09:40:07 +0300

    1st commit

---
+++ WebApiCURD/WebApiCURD/Models/DonationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCURD.Models
+{
+    public class DonationDBContext:DbContext
+    {//step 2
+        public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options)
+        {
+
+        }
+        //step 3
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
